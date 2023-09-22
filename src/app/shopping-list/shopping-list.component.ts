ab5b4dceb6799c8ae1eda186972b306628efa9d3
AbhishekGarg0507 <gargabhishek807@gmail.com>
2023-09-23 02:43:59 +0530

    add recipe cards and list-cards static

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients:Ingredient[]=[
+    new Ingredient('apple',5),
+    new Ingredient('Tomatoes',10)
+  ];
 }
